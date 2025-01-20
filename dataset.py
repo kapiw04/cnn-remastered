a847563830cdbfe18ca41099ddb9a760169f9d08
kapiw04 <wojtowicz.kacper@gmail.com>
2025-01-20 14:57:39 +0100

    dataloaders

--- dataset.py
+++ dataset.py
@@ -1,29 +1,36 @@
 import torch
 from torchvision.datasets.mnist import MNIST
 from torchvision.transforms import v2
-import matplotlib.pyplot as plt
+from torch.utils.data import DataLoader
 
-train = MNIST(root="data", train=True, download=True)  # 60000
-test = MNIST(root="data", train=False, download=True)  # 10000
 
-transform_sequence = [
-    v2.ToImage(),
-    v2.ToDtype(torch.float32, scale=True),  # Convert to tensor
-    v2.Normalize((0.1307,), (0.3081,)),  # mean and std computed for CNN
-    v2.CenterCrop(16),
-    v2.RandomResize(min_size=16, max_size=32),
-]
+def get_trainloader(batch_size):
+    transform_sequence = [
+        v2.ToImage(),
+        v2.RandomRotation(10),
+        v2.ToDtype(torch.float32, scale=True),  # Convert to tensor
+        v2.RandomResizedCrop(28, scale=(0.8, 1.0)),
+        v2.Normalize((0.1307,), (0.3081,)),  # mean and std computed for CNN
+    ]
 
-transform = v2.Compose(transform_sequence)
+    transform_pipeline = v2.Compose(transform_sequence)
 
-if __name__ == "__main__":
-    example_image = train[0][0]
+    train = MNIST(
+        root="data", train=True, download=True, transform=transform_pipeline
+    )  # 60000
+    return DataLoader(train, batch_size=batch_size, shuffle=True)
 
-    transform_sequence.append(v2.ToPILImage())
-    transform = v2.Compose(transform_sequence)
 
-    fig, ax = plt.subplots(1, 2)
-    ax[0].imshow(example_image, cmap="gray")
-    ax[1].imshow(transform(example_image), cmap="gray")
+def get_testloader(batch_size):
+    transform_sequence = [
+        v2.ToImage(),
+        v2.ToDtype(torch.float32, scale=True),  # Convert to tensor
+        v2.Normalize((0.1307,), (0.3081,)),  # mean and std computed for CNN
+    ]
 
-    plt.show()
+    transform_pipeline = v2.Compose(transform_sequence)
+
+    test = MNIST(
+        root="data", train=False, download=True, transform=transform_pipeline
+    )  # 10000
+    return DataLoader(test, batch_size=batch_size, shuffle=True)
